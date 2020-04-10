337ad7c44c952dba87d8bd9123e996f259a1a875
RK <rkmpatel8@gmail.com>
2020-04-10 19:37:56 +0530

    Initial Configurations. Created a Spring Boot Back End Application

---
+++ debut-sports/debut-sports/src/main/java/com/ds/debutsports/DebutSportsApplication.java
@@ -0,0 +1,13 @@
+package com.ds.debutsports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebutSportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebutSportsApplication.class, args);
+	}
+
+}
---
+++ debut-sports/debut-sports/src/test/java/com/ds/debutsports/DebutSportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.debutsports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebutSportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
